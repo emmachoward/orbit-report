4a6529afb4e7d253fddb942a791d80424ad2185b
Emma Howard <emmachoward@gmail.com>
2021-07-03 21:38:02 -0500

    Sorting

--- src/app/orbit-list/orbit-list.component.ts
+++ src/app/orbit-list/orbit-list.component.ts
@@ -7,11 +7,22 @@ import { Satellite } from '../satellite';
   styleUrls: ['./orbit-list.component.css']
 })
 export class OrbitListComponent implements OnInit {
-  
+
   constructor() { }
   @Input() satellites: Satellite[];
   ngOnInit() {
   }
   
+  sort(column: string): void {
+    // array.sort modifies the array, sorting the items based on the given compare function
+    this.satellites.sort(function(a: Satellite, b: Satellite): number {
+       if(a[column] < b[column]) {
+          return -1;
+       } else if (a[column] > b[column]) {
+          return 1;
+       }
+       return 0;
+    });
+ }
 
 }
